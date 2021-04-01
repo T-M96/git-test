b29bc20c3b3ce3efab9f0d3d633d91a45984b221
admin <1378387613@qq.com>
2021-04-01 12:47:21 +0800

    firstUse

---
+++ git-first-project/src/com/itheima/git/firstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("firstClass");
+        System.out.println("firstClass");
+        System.out.println("firstClass");
+        System.out.println("firstClass");
+        System.out.println("firstClass");
+        System.out.println("firstClass");
+    }
+}
